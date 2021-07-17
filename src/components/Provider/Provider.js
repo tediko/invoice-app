890db639bfd334ba3917e1a7feeb65fec6f8ce7b
tediko <tediko123@wp.pl>
2021-07-17 15:10:15 +0200

    Add: Provider component with styles and theme for colors, fonts

---
+++ src/components/Provider/Provider.js
@@ -0,0 +1,61 @@
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import { theme } from './theme';
+
+const GlobalStyle = createGlobalStyle`
+    *,
+    *::before,
+    *::after {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    html:focus-within {
+        scroll-behavior: smooth;
+    }
+
+    ul,
+    ol {
+        list-style: none;
+    }
+
+    @media (prefers-reduced-motion: reduce) {
+        html:focus-within {
+            scroll-behavior: auto;
+        }
+
+        *,
+        *::before,
+        *::after {
+            animation-duration: 0.01ms !important;
+            animation-iteration-count: 1 !important;
+            transition-duration: 0.01ms !important;
+            scroll-behavior: auto !important;
+        }
+    }
+
+    html {
+        font-size: 16px;
+    }
+
+    body {
+        font-family: ${theme.light.fonts.primary};
+        font-size: 1rem;
+    }
+
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+`;
+
+const Provider = ({ children }) => {
+    return (
+        <ThemeProvider theme={theme.light}>
+            <GlobalStyle />
+            {children}
+        </ThemeProvider>
+    );
+};
+
+export default Provider;
---
+++ src/components/Provider/theme.js
@@ -0,0 +1,29 @@
+export const theme = {
+    light: {
+        colors: {
+            white: 'hsl(0, 0%, 100%)',
+            black: 'hsl(0, 0%, 0%)',
+            purple: 'hsl(252, 94%, 67%)',
+            purpleLight: 'hsl(252, 100%, 73%)',
+            red: 'hsl(0, 80%, 63%)',
+            redLight: 'hsl(0°, 100%, 80%)',
+            green: 'hsl(160, 67%, 52%)',
+            greenLight: 'hsla(160, 67%, 52%, 5.71%)',
+            orange: 'hsl(34, 100%, 50%)',
+            orangeLight: 'hsla(34, 100%, 50%, 5.71%)',
+            gray50: 'hsl(231, 20%, 27%, 5.71%)',
+            gray100: 'hsl(231, 20%, 27%)',
+            gray200: 'hsl(231, 20%, 36%)',
+            blue50: 'hsl(231, 67%, 99%)',
+            blue200: 'hsl(231, 73%, 93%)',
+            blue400: 'hsl(231, 20%, 61%)',
+            blue600: 'hsl(231, 36%, 63%)',
+            blue800: 'hsl(231, 28%, 7%)',
+
+            bgMain: 'hsl(240, 27%, 98%)',
+        },
+        fonts: {
+            primary: `'Spartan', sans-serif`,
+        },
+    },
+};
