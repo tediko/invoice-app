ba534226448c898d815a764a82630a5f6ede792d
tediko <tediko123@wp.pl>
2021-07-17 16:23:03 +0200

    Add: aria-label for Logo, ThemeToggle and remove Container

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,30 +1,22 @@
 import Icon from '../Icon/Icon';
 import { useTheme } from 'styled-components';
-import {
-    StyledHeader,
-    Container,
-    Logo,
-    ThemeToggle,
-    Profile,
-} from './HeaderStyles';
+import { StyledHeader, Logo, ThemeToggle, Profile } from './HeaderStyles';
 
 const Header = () => {
     const theme = useTheme();
 
     return (
         <StyledHeader>
-            <Container>
-                <Logo />
-                <ThemeToggle>
-                    <Icon
-                        name="moon"
-                        size={20}
-                        color={theme.colors.blue600}
-                        customStyle={{ transition: 'color 350ms ease-in-out' }}
-                    />
-                </ThemeToggle>
-                <Profile />
-            </Container>
+            <Logo aria-label="Home Page" />
+            <ThemeToggle aria-label="Theme toggle">
+                <Icon
+                    name="moon"
+                    size={20}
+                    color={theme.colors.blue600}
+                    customStyle={{ transition: 'color 350ms ease-in-out' }}
+                />
+            </ThemeToggle>
+            <Profile />
         </StyledHeader>
     );
 };
