7ef75b6e9454f10f3aa71535b3fcacb5344975ad
tediko <tediko123@wp.pl>
2021-07-18 16:42:57 +0200

    Add: Imported useThemeToggle and use it values to AppProvider

--- src/components/App/context.js
+++ src/components/App/context.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect, useContext } from 'react';
 import Provider from '../Provider/Provider';
+import useThemeToggle from './useThemeToggle';
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
+    const { theme, toggleTheme } = useThemeToggle();
+
     return (
-        <AppContext.Provider value="">
-            <Provider>{children}</Provider>
+        <AppContext.Provider value={{ theme, toggleTheme }}>
+            <Provider themeColor={theme}>{children}</Provider>
         </AppContext.Provider>
     );
 };
