079d7183c538c69807e63cede0f15010711c779c
tediko <tediko123@wp.pl>
2021-07-18 14:04:55 +0200

    Add: transition for background

--- src/components/Wrapper/WrapperStyles.js
+++ src/components/Wrapper/WrapperStyles.js
@@ -2,5 +2,6 @@ import styled from 'styled-components';
 
 export const StyledWrapper = styled.div`
     min-height: 100vh;
-    background: ${(props) => props.theme.colors.bgMain};
+    background-color: ${(props) => props.theme.colors.bgMain};
+    transition: background-color 400ms ease-in-out;
 `;
