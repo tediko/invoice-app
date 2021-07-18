c36d28df215130a3b6117d0aed7f0a33ed325a07
tediko <tediko123@wp.pl>
2021-07-18 14:20:07 +0200

    Add: onClick function to ThemeToggle and change Icon based on theme state

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,20 @@
 import Icon from '../Icon/Icon';
 import { useTheme } from 'styled-components';
 import { StyledHeader, Logo, ThemeToggle, Profile } from './HeaderStyles';
+import { useGlobalContext } from '../App/context';
 
 const Header = () => {
-    const theme = useTheme();
-    console.log(theme);
+    const { colors } = useTheme();
+    const { theme, toggleTheme } = useGlobalContext();
+
     return (
         <StyledHeader>
             <Logo aria-label="Home Page" />
-            <ThemeToggle aria-label="Theme toggle">
+            <ThemeToggle aria-label="Theme toggle" onClick={toggleTheme}>
                 <Icon
-                    name="moon"
+                    name={theme === 'light' ? 'moon' : 'sun'}
                     size={20}
-                    color={theme.colors.btnTheme}
+                    color={colors.btnTheme}
                     customStyle={{ transition: 'color 350ms ease-in-out' }}
                 />
             </ThemeToggle>
