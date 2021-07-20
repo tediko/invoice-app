cd4fde086a04e94ac11bf95be09bc4f5377ffee2
tediko <tediko123@wp.pl>
2021-07-20 14:01:40 +0200

    Add: Invoices component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import Wrapper from '../Wrapper/Wrapper';
 import Header from '../Header/Header';
+import Invoices from '../Invoices/Invoices';
 
 const App = () => {
     return (
         <Wrapper>
             <Header />
+            <Invoices />
         </Wrapper>
     );
 };
