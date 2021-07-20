d0f3e7f69fcddbff56a106c52f426a6ba530f904
tediko <tediko123@wp.pl>
2021-07-20 14:13:25 +0200

    Add: Flex to StyledWrapper and desktop breakpoint with flex-flow

--- src/components/Wrapper/WrapperStyles.js
+++ src/components/Wrapper/WrapperStyles.js
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
 
 export const StyledWrapper = styled.div`
+    display: flex;
+    flex-flow: column;
     min-height: 100vh;
     background-color: ${(props) => props.theme.colors.bgMain};
     transition: background-color 400ms ease-in-out;
+
+    @media (min-width: 1024px) {
+        flex-flow: row;
+    }
 `;
