94080a39dd6f6b20a0edc9be75def64e21d8b68f
tediko <tediko123@wp.pl>
2021-07-20 17:25:06 +0200

    Add: Shared typography with font styles

---
+++ src/components/shared/Typography.js
@@ -0,0 +1,15 @@
+import styled, { css } from 'styled-components';
+
+export const primaryFontStyles = css`
+    font-family: 'Spartan', sans-serif;
+    font-size: 0.75rem;
+    letter-spacing: -0.016rem;
+    line-height: 1.25;
+`;
+
+export const secondaryFontStyles = css`
+    font-family: 'Spartan', sans-serif;
+    font-size: 0.6875rem;
+    letter-spacing: -0.014rem;
+    line-height: 1.63;
+`;
