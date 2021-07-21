319517963f6fba2ffbbc0698b27e817fc30f0243
tediko <tediko123@wp.pl>
2021-07-21 11:14:02 +0200

    Add: windowWidth state and pass it to AppContext.Provider

--- src/components/App/context.js
+++ src/components/App/context.js
@@ -6,9 +6,27 @@ const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
     const { theme, toggleTheme } = useThemeToggle();
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    /**
+     * Listen for window resize and call handleResize function
+     */
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    /**
+     * Assign window width value to a windowWidth state.
+     */
+    const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+    };
 
     return (
-        <AppContext.Provider value={{ theme, toggleTheme }}>
+        <AppContext.Provider value={{ theme, toggleTheme, windowWidth }}>
             <Provider themeColor={theme}>{children}</Provider>
         </AppContext.Provider>
     );
