1f19be4a22355bf7002c78e04c9a15a0119a9abc
tediko <tediko123@wp.pl>
2021-07-26 15:23:10 +0200

    Add: Mobile styles for Summary elements

--- src/components/InvoiceInfo/InvoiceInfo.js
+++ src/components/InvoiceInfo/InvoiceInfo.js
@@ -1,3 +1,4 @@
+import { useGlobalContext } from '../App/context';
 import {
     Info,
     InfoContainer,
@@ -12,9 +13,23 @@ import {
     Text,
     SenderAddress,
     ClientAddress,
+    Summary,
+    SummaryContainer,
+    SummaryItem,
+    SummaryHeading,
+    SummaryText,
+    ItemName,
+    ItemQty,
+    ItemPrice,
+    ItemTotal,
+    Total,
+    TotalHeading,
+    TotalText,
 } from '../InvoiceInfo/InvoiceInfoStyles';
 
 const InvoiceInfo = () => {
+    const { windowWidth } = useGlobalContext();
+    const isDesktop = windowWidth >= 768;
     return (
         <Info>
             <InfoContainer>
@@ -53,6 +68,26 @@ const InvoiceInfo = () => {
                     <Text>20 Sep 2021</Text>
                 </PaymentDue>
             </InfoContainer>
+            <Summary>
+                <SummaryContainer>
+                    <SummaryItem>
+                        <ItemName>Banner Design</ItemName>
+                        <ItemQty>1 {!isDesktop && ' x £ 156.00'}</ItemQty>
+                        {isDesktop && <ItemPrice>£ 156.00</ItemPrice>}
+                        <ItemTotal>£ 156.00</ItemTotal>
+                    </SummaryItem>
+                    <SummaryItem>
+                        <ItemName>Email Design</ItemName>
+                        <ItemQty>2 {!isDesktop && ' x £ 200.00'}</ItemQty>
+                        {isDesktop && <ItemPrice>£ 200.00</ItemPrice>}
+                        <ItemTotal>£ 400.00</ItemTotal>
+                    </SummaryItem>
+                </SummaryContainer>
+            </Summary>
+            <Total>
+                <TotalHeading>Amount Due</TotalHeading>
+                <TotalText>£ 556.00</TotalText>
+            </Total>
         </Info>
     );
 };
--- src/components/InvoiceInfo/InvoiceInfoStyles.js
+++ src/components/InvoiceInfo/InvoiceInfoStyles.js
@@ -1,12 +1,17 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { primaryFontStyles, secondaryFontStyles } from '../shared/Typography';
-import { headingSmall, headingMediumSmall } from '../shared/Headings';
+import {
+    headingSmall,
+    headingMediumSmall,
+    headingLargeSmall,
+} from '../shared/Headings';
 
 export const Info = styled.article`
     background-color: ${({ theme }) => theme.colors.bgView};
     transition: background-color 400ms ease-in-out;
     border-radius: 8px;
     box-shadow: 0 10px 10px -10px ${({ theme }) => theme.colors.bgViewShadow};
+    padding: 24px;
 `;
 
 export const InfoContainer = styled.div`
@@ -14,7 +19,7 @@ export const InfoContainer = styled.div`
     grid-template-columns: 1fr 1fr;
     grid-template-areas: 'key .' 'senderAddress .' 'createdDate clientInfo' 'paymentDue clientInfo' 'email email';
     gap: 30px;
-    padding: 24px;
+    margin-bottom: 40px;
 `;
 
 export const Key = styled.div`
@@ -80,3 +85,76 @@ export const SenderAddress = styled(Address)`
 `;
 
 export const ClientAddress = styled(Address)``;
+
+export const Summary = styled.div`
+    background-color: ${({ theme }) => theme.colors.bgViewSummary};
+    transition: background-color 400ms ease-in-out;
+    border-radius: 8px 8px 0 0;
+`;
+
+export const SummaryContainer = styled.div`
+    display: flex;
+    flex-flow: column;
+    gap: 24px;
+    padding: 24px;
+`;
+
+export const SummaryItem = styled.div`
+    display: grid;
+    grid-template-columns: 1fr auto;
+    grid-template-areas: 'name total' 'qty total';
+    gap: 10px;
+    align-items: center;
+`;
+
+export const SummaryHeading = styled.h5`
+    ${primaryFontStyles}
+    color: ${({ theme }) => theme.colors.textSecondary};
+    transition: color 400ms ease-in-out;
+`;
+
+export const SummaryText = styled.p`
+    ${primaryFontStyles}
+    font-weight: 600;
+    color: ${({ theme }) => theme.colors.textPrimary};
+    transition: color 400ms ease-in-out;
+`;
+
+export const ItemName = styled(SummaryText)`
+    grid-area: name;
+`;
+
+export const ItemQty = styled(SummaryText)`
+    grid-area: qty;
+    color: ${({ theme }) => theme.colors.textSecondary};
+`;
+
+export const ItemPrice = styled(SummaryText)`
+    grid-area: price;
+    color: ${({ theme }) => theme.colors.textSecondary};
+`;
+
+export const ItemTotal = styled(SummaryText)`
+    grid-area: total;
+`;
+
+export const Total = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: ${({ theme }) => theme.colors.bgViewSummaryFooter};
+    transition: background-color 400ms ease-in-out;
+    border-radius: 0 0 8px 8px;
+    padding: 24px 32px;
+`;
+
+export const TotalHeading = styled.h5`
+    ${secondaryFontStyles}
+    color: ${({ theme }) => theme.colors.white};
+    font-weight: 400;
+`;
+
+export const TotalText = styled.p`
+    ${headingLargeSmall}
+    color: ${({ theme }) => theme.colors.white};
+`;
