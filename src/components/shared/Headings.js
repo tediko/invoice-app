3b2512990f27a19480c70bcc781c4ce34a72957c
tediko <tediko123@wp.pl>
2021-07-26 15:07:43 +0200

    Add: headingLargeSmall, headingExtraSmall

--- src/components/shared/Headings.js
+++ src/components/shared/Headings.js
@@ -19,6 +19,24 @@ export const headingTitle = css`
 
 export const headingLarge = css`
     ${commonStyles}
+    font-size: 1.5rem;
+    letter-spacing: -0.03125rem;
+    line-height: 2.14;
+    color: ${({ theme }) => theme.colors.textPrimary};
+    transition: color 400ms ease-in-out;
+`;
+
+export const headingLargeSmall = css`
+    ${commonStyles}
+    font-size: clamp(1.25rem, 5vw, 1.5rem);
+    letter-spacing: -0.026rem;
+    line-height: 2.14;
+    color: ${({ theme }) => theme.colors.textPrimary};
+    transition: color 400ms ease-in-out;
+
+    @media (min-width: 768px) {
+        letter-spacing: -0.03125rem;
+    }
 `;
 
 export const headingBig = css`
@@ -38,7 +56,30 @@ export const headingMedium = css`
     transition: color 400ms ease-in-out;
 `;
 
+export const headingMediumSmall = css`
+    ${commonStyles}
+    font-size: 0.9375rem;
+    letter-spacing: -0.019rem;
+    line-height: 1;
+    color: ${({ theme }) => theme.colors.textPrimary};
+    transition: color 400ms ease-in-out;
+`;
+
 export const headingSmall = css`
+    ${commonStyles}
+    font-size: clamp(0.75rem, 3vw, 1rem);
+    letter-spacing: -0.016rem;
+    line-height: 1.25;
+    color: ${({ theme }) => theme.colors.textPrimary};
+    transition: color 400ms ease-in-out;
+
+    @media (min-width: 768px) {
+        letter-spacing: -0.05rem;
+        line-height: 1.5;
+    }
+`;
+
+export const headingExtraSmall = css`
     ${commonStyles}
     font-size: 0.75rem;
     letter-spacing: -0.016rem;
