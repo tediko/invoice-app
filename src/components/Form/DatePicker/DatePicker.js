eada5669bdaaad2851eb19e57cfe0bcab8aa0418
tediko <tediko123@wp.pl>
2021-08-03 16:25:29 +0200

    Remove: useTheme() so it doesn't re-render component

--- src/components/Form/DatePicker/DatePicker.js
+++ src/components/Form/DatePicker/DatePicker.js
@@ -7,13 +7,12 @@ import Icon from '../../shared/Icon/Icon';
 import { CustomPicker } from './DatePickerStyles';
 
 const DatePicker = () => {
-    const { colors } = useTheme();
     const [startDate, setStartDate] = useState(new Date());
 
     const CustomInput = forwardRef(({ value, onClick }, ref) => (
         <CustomPicker type="button" onClick={onClick} value={value} ref={ref}>
             {dateToString(value)}
-            <Icon name={'calendar'} size={12} color={colors.blueGrayish} />
+            <Icon name={'calendar'} size={12} color="hsl(252, 94%, 67%)" />
         </CustomPicker>
     ));
     return (
