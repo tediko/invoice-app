5a02e550804362d1846ab2bc72116bbfa5389b68
tediko <tediko123@wp.pl>
2021-08-05 16:43:06 +0200

    Add: state.invoices for map instead of data from file

--- src/components/Invoices/List/List.js
+++ src/components/Invoices/List/List.js
@@ -1,5 +1,4 @@
 import { useTheme } from 'styled-components';
-import data from './data.json';
 import Icon from '../../shared/Icon/Icon';
 import Status from '../../shared/Status/Status';
 import languageSensitiveNum from '../../../utilities/languageSensitiveNum';
@@ -18,34 +17,38 @@ import {
 
 const List = () => {
     const { colors } = useTheme();
-    const { windowWidth } = useGlobalContext();
+    const { state, windowWidth } = useGlobalContext();
     const isDesktop = windowWidth >= 768;
 
     return (
         <StyledList>
-            {data.map(({ id, paymentDue, clientName, status, total }) => (
-                <Item key={id}>
-                    <Link>
-                        <Uid>
-                            <Hashtag>#</Hashtag>
-                            {id}
-                        </Uid>
-                        <PaymentDue>Due {dateToString(paymentDue)}</PaymentDue>
-                        <ClientName>{clientName}</ClientName>
-                        <TotalPrice>
-                            £&nbsp;{languageSensitiveNum(total)}
-                        </TotalPrice>
-                        <Status currStatus={status} $grid />
-                        {isDesktop && (
-                            <Icon
-                                name={'arrow-right'}
-                                size={10}
-                                color={colors.purple}
-                            />
-                        )}
-                    </Link>
-                </Item>
-            ))}
+            {state.invoices.map(
+                ({ id, paymentDue, clientName, status, total }) => (
+                    <Item key={id}>
+                        <Link>
+                            <Uid>
+                                <Hashtag>#</Hashtag>
+                                {id}
+                            </Uid>
+                            <PaymentDue>
+                                Due {dateToString(paymentDue)}
+                            </PaymentDue>
+                            <ClientName>{clientName}</ClientName>
+                            <TotalPrice>
+                                £&nbsp;{languageSensitiveNum(total)}
+                            </TotalPrice>
+                            <Status currStatus={status} $grid />
+                            {isDesktop && (
+                                <Icon
+                                    name={'arrow-right'}
+                                    size={10}
+                                    color={colors.purple}
+                                />
+                            )}
+                        </Link>
+                    </Item>
+                )
+            )}
         </StyledList>
     );
 };
