7a9b22ecdd192e3a2a33f07ce4d115cd5da408b2
tediko <tediko123@wp.pl>
2021-08-05 16:31:47 +0200

    Change: Move forwardRef outside DatePicker component so it doesn't re-render

--- src/components/Form/DatePicker/DatePicker.js
+++ src/components/Form/DatePicker/DatePicker.js
@@ -6,15 +6,16 @@ import { useGlobalContext } from '../../App/context';
 import Icon from '../../shared/Icon/Icon';
 import { CustomPicker } from './DatePickerStyles';
 
+const CustomInput = forwardRef(({ value, onClick }, ref) => (
+    <CustomPicker type="button" onClick={onClick} value={value} ref={ref}>
+        {dateToString(value)}
+        <Icon name={'calendar'} size={12} color="hsl(252, 94%, 67%)" />
+    </CustomPicker>
+));
+
 const DatePicker = () => {
     const { invoice, handleDatePickerChange } = useGlobalContext();
 
-    const CustomInput = forwardRef(({ value, onClick }, ref) => (
-        <CustomPicker type="button" onClick={onClick} value={value} ref={ref}>
-            {dateToString(value)}
-            <Icon name={'calendar'} size={12} color="hsl(252, 94%, 67%)" />
-        </CustomPicker>
-    ));
     return (
         <ReactDatePicker
             selected={invoice.createdAt}
