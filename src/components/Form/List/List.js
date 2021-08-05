2790d96efb48346435eff30491ea3faa5719a94e
tediko <tediko123@wp.pl>
2021-08-05 16:44:39 +0200

    Add: map for items, handlers and names for inputs from useGlobalContext

--- src/components/Form/List/List.js
+++ src/components/Form/List/List.js
@@ -1,6 +1,7 @@
 import Icon from '../../shared/Icon/Icon';
 import Button from '../../shared/Button/Button';
 import { useTheme } from 'styled-components';
+import { useGlobalContext } from '../../App/context';
 import {
     Fieldset,
     Legend,
@@ -14,32 +15,60 @@ import {
 
 const List = () => {
     const { colors } = useTheme();
+    const { items, handleItemsChange, handleItemsAdd, handleItemsRemove } =
+        useGlobalContext();
 
     return (
         <Fieldset>
             <Legend $lg>Item List</Legend>
-            <InputsGroup>
-                <InputWrapper>
-                    <Label>Item Name</Label>
-                    <Input type="text" />
-                </InputWrapper>
-                <InputWrapper>
-                    <Label>Qty.</Label>
-                    <Input type="text" />
-                </InputWrapper>
-                <InputWrapper>
-                    <Label>Price</Label>
-                    <Input type="text" />
-                </InputWrapper>
-                <InputWrapper>
-                    <Label>Total</Label>
-                    <TotalValue>173.00</TotalValue>
-                </InputWrapper>
-                <Delete type="button">
-                    <Icon name="delete" size={16} color={colors.btnTheme} />
-                </Delete>
-            </InputsGroup>
-            <Button type="button" $secondary>
+            {items.map((item, index) => (
+                <InputsGroup key={index}>
+                    <InputWrapper>
+                        <Label htmlFor="name">Item Name</Label>
+                        <Input
+                            type="text"
+                            name="name"
+                            value={item.name}
+                            onChange={(event) =>
+                                handleItemsChange(index, event)
+                            }
+                        />
+                    </InputWrapper>
+                    <InputWrapper>
+                        <Label htmlFor="quantity">Qty.</Label>
+                        <Input
+                            type="text"
+                            name="quantity"
+                            value={item.quantity}
+                            onChange={(event) =>
+                                handleItemsChange(index, event)
+                            }
+                        />
+                    </InputWrapper>
+                    <InputWrapper>
+                        <Label htmlFor="price">Price</Label>
+                        <Input
+                            type="text"
+                            name="price"
+                            value={item.price}
+                            onChange={(event) =>
+                                handleItemsChange(index, event)
+                            }
+                        />
+                    </InputWrapper>
+                    <InputWrapper>
+                        <Label>Total</Label>
+                        <TotalValue>{item.total}</TotalValue>
+                    </InputWrapper>
+                    <Delete
+                        type="button"
+                        onClick={() => handleItemsRemove(index)}
+                    >
+                        <Icon name="delete" size={16} color={colors.btnTheme} />
+                    </Delete>
+                </InputsGroup>
+            ))}
+            <Button type="button" $secondary onClick={handleItemsAdd}>
                 + Add New Item
             </Button>
         </Fieldset>
