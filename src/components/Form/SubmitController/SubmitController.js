b7caed5b55e22ea6072f847e6434b2e7db233a25
tediko <tediko123@wp.pl>
2021-08-05 16:45:54 +0200

    Add: handleSubmit to Button from useGlobalContext

--- src/components/Form/SubmitController/SubmitController.js
+++ src/components/Form/SubmitController/SubmitController.js
@@ -1,16 +1,29 @@
 import Button from '../../shared/Button/Button';
+import { useGlobalContext } from '../../App/context';
 import { StyledSubmitController } from './SubmitControllerStyles';
 
 const SubmitController = () => {
+    const { handleSubmit } = useGlobalContext();
+
     return (
         <StyledSubmitController>
             <Button $small type="button" $secondary>
                 Discard
             </Button>
-            <Button $small type="submit" $save>
+            <Button
+                $small
+                type="submit"
+                $save
+                onClick={(event) => handleSubmit(event, 'save')}
+            >
                 Save as Draft
             </Button>
-            <Button $small type="submit" $primary>
+            <Button
+                $small
+                type="submit"
+                $primary
+                onClick={(event) => handleSubmit(event, 'add')}
+            >
                 Save & Send
             </Button>
         </StyledSubmitController>
