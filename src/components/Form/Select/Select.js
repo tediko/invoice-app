d159c975c901b8efc711ae649d71504a50ca3b41
tediko <tediko123@wp.pl>
2021-08-05 16:45:28 +0200

    Add: handlers to Option's from useGlobalContext

--- src/components/Form/Select/Select.js
+++ src/components/Form/Select/Select.js
@@ -1,9 +1,11 @@
 import Icon from '../../shared/Icon/Icon';
 import { useState } from 'react';
 import { useTheme } from 'styled-components';
+import { useGlobalContext } from '../../App/context';
 import { StyledSelect, Cta, List, Item, Option } from './SelectStyles';
 
 const Select = () => {
+    const { invoice, handleInvoiceChange } = useGlobalContext();
     const { colors } = useTheme();
     const [isExpanded, setIsExpanded] = useState(false);
 
@@ -21,28 +23,48 @@ const Select = () => {
                 onClick={expandSelect}
                 $isExpanded={isExpanded}
             >
-                Net 30 Days
+                Net {invoice.paymentTerms} Days
                 <Icon name={'arrow-down'} size={12} color={colors.purple} />
             </Cta>
             {isExpanded && (
                 <List id="select-list">
                     <Item>
-                        <Option type="button" value="1">
+                        <Option
+                            type="button"
+                            name="paymentTerms"
+                            value="1"
+                            onClick={(event) => handleInvoiceChange(event)}
+                        >
                             Net 1 Days
                         </Option>
                     </Item>
                     <Item>
-                        <Option type="button" value="7">
+                        <Option
+                            type="button"
+                            name="paymentTerms"
+                            value="7"
+                            onClick={(event) => handleInvoiceChange(event)}
+                        >
                             Net 7 Days
                         </Option>
                     </Item>
                     <Item>
-                        <Option type="button" value="14">
+                        <Option
+                            type="button"
+                            name="paymentTerms"
+                            value="14"
+                            onClick={(event) => handleInvoiceChange(event)}
+                        >
                             Net 14 Days
                         </Option>
                     </Item>
                     <Item>
-                        <Option type="button" value="30">
+                        <Option
+                            type="button"
+                            name="paymentTerms"
+                            value="30"
+                            onClick={(event) => handleInvoiceChange(event)}
+                        >
                             Net 30 Days
                         </Option>
                     </Item>
