e9eda632fc475abe5273938b4b6fb541dd411af7
tediko <tediko123@wp.pl>
2021-08-06 15:11:04 +0200

    Add: Functionality to Button, List, StatusFilter

--- src/components/Invoices/Filter/Filter.js
+++ src/components/Invoices/Filter/Filter.js
@@ -1,13 +1,40 @@
+import { useState, useEffect, useRef } from 'react';
 import Icon from '../../shared/Icon/Icon';
 import { useTheme } from 'styled-components';
+import { useGlobalContext } from '../../App/context';
 import { StyledFilter, Button, List, Item, StatusFilter } from './FilterStyles';
 
 const Filter = ({ isDesktop }) => {
     const { colors } = useTheme();
+    const { filterType, changeFilterType } = useGlobalContext();
+    const [isFilterOpen, setIsFilterOpen] = useState(false);
+    const ref = useRef();
+
+    /* Running an effect whenever isFilterOpen changes and we are binding a click event to the document 
+    so that whenever the user clicks on the document, we can check if it is inside or outside the list 
+    and hide the list accordingly. */
+    useEffect(() => {
+        const checkIfClickedOutside = (event) => {
+            const target = event.target;
+            if (isFilterOpen && ref.current && !ref.current.contains(target)) {
+                setIsFilterOpen(false);
+            }
+        };
+        document.addEventListener('click', checkIfClickedOutside);
+
+        return () => {
+            document.removeEventListener('click', checkIfClickedOutside);
+        };
+    }, [isFilterOpen]);
+
+    /* Function that toggle filter list.  */
+    const toggleFilterList = () => {
+        setIsFilterOpen(!isFilterOpen);
+    };
 
     return (
         <StyledFilter>
-            <Button>
+            <Button onClick={toggleFilterList}>
                 Filter {isDesktop && 'by status'}
                 <Icon
                     name="arrow-down"
@@ -18,17 +45,37 @@ const Filter = ({ isDesktop }) => {
                     }}
                 />
             </Button>
-            <List>
-                <Item>
-                    <StatusFilter>Draft</StatusFilter>
-                </Item>
-                <Item>
-                    <StatusFilter>Pending</StatusFilter>
-                </Item>
-                <Item>
-                    <StatusFilter>Paid</StatusFilter>
-                </Item>
-            </List>
+            {isFilterOpen && (
+                <List ref={ref}>
+                    <Item>
+                        <StatusFilter
+                            onClick={(event) => changeFilterType(event)}
+                            value="draft"
+                            $isActive={filterType === 'draft'}
+                        >
+                            Draft
+                        </StatusFilter>
+                    </Item>
+                    <Item>
+                        <StatusFilter
+                            onClick={(event) => changeFilterType(event)}
+                            value="pending"
+                            $isActive={filterType === 'pending'}
+                        >
+                            Pending
+                        </StatusFilter>
+                    </Item>
+                    <Item>
+                        <StatusFilter
+                            onClick={(event) => changeFilterType(event)}
+                            value="paid"
+                            $isActive={filterType === 'paid'}
+                        >
+                            Paid
+                        </StatusFilter>
+                    </Item>
+                </List>
+            )}
         </StyledFilter>
     );
 };
