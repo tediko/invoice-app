693d97aea536d82e0284258aeb31180018a25a7a
tediko <tediko123@wp.pl>
2021-08-06 16:41:47 +0200

    Add: Function to generate message about invoices amount

---
+++ src/utilities/invoicesLengthMessage.js
@@ -0,0 +1,28 @@
+/**
+ * Function to generate message about invoices amount.
+ * @param {array} invoicesArr Array with items
+ * @param {string} filterType Type of current filter
+ * @param {number} windowWidth Number with window width
+ * @return {string} Formatted string.
+ */
+export const invoicesLengthMessage = (invoicesArr, filterType, windowWidth) => {
+    const invoicesLength = invoicesArr.length;
+    const isMobile = windowWidth <= 768;
+    const type = filterType === 'all' ? 'total' : filterType;
+
+    if (invoicesLength === 0) {
+        return `No Invoices`;
+    } else if (invoicesLength === 1) {
+        if (isMobile) {
+            return `1 ${type} invoice`;
+        } else {
+            return `There is 1 ${type} invoice`;
+        }
+    } else if (invoicesLength > 1) {
+        if (isMobile) {
+            return `${invoicesLength} ${type} invoices`;
+        } else {
+            return `There are ${invoicesLength} ${type} invoices`;
+        }
+    }
+};
