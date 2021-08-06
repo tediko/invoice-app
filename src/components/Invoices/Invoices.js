e832ac8ffe12473d2a96c79521c7f3574950e42c
tediko <tediko123@wp.pl>
2021-08-06 16:45:34 +0200

    Add: invoiceLengthMessage function for Text and return ErrorMessage or List based on isEmpty variable

--- src/components/Invoices/Invoices.js
+++ src/components/Invoices/Invoices.js
@@ -2,25 +2,35 @@ import { useGlobalContext } from '../App/context';
 import Filter from './Filter/Filter';
 import List from './List/List';
 import Button from '../shared/Button/Button';
+import ErrorMessage from './ErrorMessage/ErrorMessage';
+import { invoicesLengthMessage } from '../../utilities/invoicesLengthMessage';
 import { Container, Header, Info, Title, Text } from './InvoicesStyles';
 
 const Invoices = () => {
-    const { windowWidth, openForm } = useGlobalContext();
+    const { windowWidth, openForm, filteredInvoices, filterType } =
+        useGlobalContext();
     const isDesktop = windowWidth >= 768;
+    const isEmpty = filteredInvoices.length === 0;
 
     return (
         <Container>
             <Header>
                 <Info>
                     <Title>Invoices</Title>
-                    <Text>7 invoices</Text>
+                    <Text>
+                        {invoicesLengthMessage(
+                            filteredInvoices,
+                            filterType,
+                            windowWidth
+                        )}
+                    </Text>
                 </Info>
                 <Filter isDesktop={isDesktop} />
                 <Button type="button" $newInvoice onClick={openForm}>
                     New {isDesktop && 'Invoice'}
                 </Button>
             </Header>
-            <List />
+            {isEmpty ? <ErrorMessage /> : <List />}
         </Container>
     );
 };
