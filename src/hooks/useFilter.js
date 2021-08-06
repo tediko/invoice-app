8c1cb01a50283f8038db1bd1bf61ed13f43c0253
tediko <tediko123@wp.pl>
2021-08-06 15:54:51 +0200

    Add: Custom hook useFilter

---
+++ src/hooks/useFilter.js
@@ -0,0 +1,57 @@
+import { useEffect, useState, useRef } from 'react';
+
+const initialFilterType = 'all';
+
+const useFilter = (callback) => {
+    const [filteredInvoices, setFilteredInvoices] = useState(callback.invoices);
+    const [filterType, setFilterType] = useState(initialFilterType);
+    const previousFilterType = useRef(initialFilterType);
+
+    /**
+     * Running an effect whenever callback changes and we are calling handleFilter with current type to
+     * return a filtered list.
+     */
+    useEffect(() => {
+        handleFilter(filterType);
+    }, [callback]);
+
+    /**
+     * Function to filter filteredInvoices based on filter type.
+     * @param    {String}  string - String with filter type
+     */
+    const handleFilter = (type) => {
+        if (type === initialFilterType) {
+            setFilteredInvoices(callback.invoices);
+            return false;
+        }
+
+        const newInvoices = callback.invoices.filter(
+            (item) => item.status === type
+        );
+        setFilteredInvoices(newInvoices);
+    };
+
+    /**
+     * Function to change filter type based on passed props.
+     * Call handleFilter function to change filteredInvoices based on that filter type.
+     * @param    {String}  event - String with filter type
+     */
+    const changeFilterType = (event) => {
+        const type = event.target.value;
+
+        if (previousFilterType.current === type) {
+            previousFilterType.current = initialFilterType;
+            handleFilter(initialFilterType);
+            setFilterType(initialFilterType);
+            return false;
+        }
+
+        previousFilterType.current = type;
+        handleFilter(type);
+        setFilterType(type);
+    };
+
+    return { filteredInvoices, filterType, handleFilter, changeFilterType };
+};
+
+export default useFilter;
