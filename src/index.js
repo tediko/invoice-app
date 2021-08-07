dbf2cb43d817555f59d36cb94fa682d4809e32ba
tediko <tediko123@wp.pl>
2021-08-07 12:11:28 +0200

    Add: Wrap App component with Router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App/App';
 import { AppProvider } from './components/App/context';
 
 ReactDOM.render(
-    <AppProvider>
-        <App />
-    </AppProvider>,
+    <Router>
+        <AppProvider>
+            <App />
+        </AppProvider>
+    </Router>,
     document.getElementById('root')
 );
