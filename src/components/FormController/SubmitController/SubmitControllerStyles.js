90c38a921f2d5e4456c88709de81b6692b5e4e20
tediko <tediko123@wp.pl>
2021-08-15 13:02:09 +0200

    Change: Update parent folder name

---
+++ src/components/FormController/SubmitController/SubmitControllerStyles.js
@@ -0,0 +1,32 @@
+import styled, { css } from 'styled-components';
+
+export const StyledSubmitController = styled.div`
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    justify-content: end;
+    gap: 7px;
+    padding: 24px;
+    background-color: ${({ theme }) => theme.colors.bgForm};
+    z-index: 3;
+    transition: background-color 400ms ease-in-out;
+
+    ${({ $isEdited }) =>
+        !$isEdited &&
+        css`
+            & > :first-child {
+                margin-right: auto;
+            }
+        `}
+
+    @media (min-width: 768px) {
+        padding: 32px 56px;
+        border-radius: 20px 20px 0 0;
+    }
+
+    @media (min-width: 1024px) {
+        padding: 32px 56px 32px 159px;
+    }
+`;
