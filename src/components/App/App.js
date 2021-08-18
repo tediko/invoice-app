891b3ad05f9281432e3bad6ed5109040e7b5ee82
tediko <tediko123@wp.pl>
2021-08-18 17:29:48 +0200

    Add: App structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,23 @@
+import { Route, Switch } from 'react-router-dom';
 import Wrapper from '../Wrapper/Wrapper';
 import Header from '../Header/Header';
 import Invoices from '../Invoices/Invoices';
+import FormController from '../FormController/FormController';
+import InvoiceView from '../InvoiceView/InvoiceView';
+import Modal from '../Modal/Modal';
 
 const App = () => {
     return (
         <Wrapper>
+            <FormController />
+            <Modal />
             <Header />
-            <Invoices />
+            <Switch>
+                <Route exact path="/">
+                    <Invoices />
+                </Route>
+                <Route path="/invoice/:id" children={<InvoiceView />} />
+            </Switch>
         </Wrapper>
     );
 };
