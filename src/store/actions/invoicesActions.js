51c43dbb8c743035b043dd283d159c478c7920bf
tediko <tediko123@wp.pl>
2021-08-18 17:28:48 +0200

    Add: comments

--- src/store/actions/invoicesActions.js
+++ src/store/actions/invoicesActions.js
@@ -2,6 +2,14 @@ import * as ACTION_TYPES from '../actions/action_type';
 import generateUniqueId from '../../utilities/generateUniqueID';
 import generatePaymentDueDate from '../../utilities/generatePaymentDueDate';
 
+// REDUCER ACTIONS
+/**
+ * Function to add invoice to state. In payload we're passing new invoice, new unique ID, paymentDue and status.
+ * If passed type is equal to 'new' return 'pending' status otherwise 'draft'.
+ * @param    {object} invoice    Object of created invoice
+ * @param    {string} state    Object of state
+ * @param    {string} type    String with type of invoice. (available: 'new' or 'draft')
+ */
 export const add = (invoice, state, type) => {
     return {
         type: ACTION_TYPES.ADD_INVOICE,
@@ -17,6 +25,10 @@ export const add = (invoice, state, type) => {
     };
 };
 
+/**
+ * Function to save changes on editing invoice. In payload we're passing new invoice, paymentDue and status 'pending'
+ * @param    {object} invoice    Object of edited invoice
+ */
 export const change = (invoice) => {
     return {
         type: ACTION_TYPES.SAVE_CHANGES,
@@ -31,6 +43,10 @@ export const change = (invoice) => {
     };
 };
 
+/**
+ * Function to toggle form to edit existing invoice. In payload we're passing id of edited invoivce.
+ * @param    {string} index    String with invoice index
+ */
 export const edit = (index) => {
     return {
         type: ACTION_TYPES.EDIT,
@@ -38,30 +54,48 @@ export const edit = (index) => {
     };
 };
 
+/**
+ * Function to delete invoice.
+ */
 export const remove = () => {
     return {
         type: ACTION_TYPES.DELETE,
     };
 };
 
+/**
+ * Function to change status of invoice to 'paid'.
+ */
 export const paid = () => {
     return {
         type: ACTION_TYPES.PAID,
     };
 };
 
+/**
+ * Function to toggle form to create new invoice.
+ */
 export const create = () => {
     return {
         type: ACTION_TYPES.CREATE,
     };
 };
 
+/**
+ * Function to discard/hide form.
+ */
 export const discard = () => {
     return {
         type: ACTION_TYPES.DISCARD,
     };
 };
 
+/**
+ * Function to toggle modal. In payload we're passing name of modal to display aswell as id of index
+ * for which the modal is to be displayed.
+ * @param    {string} index    String with invoice index
+ * @param    {string} name    String with name of modal (available: 'delete' or 'status')
+ */
 export const modal = (index, name) => {
     return {
         type: ACTION_TYPES.TOGGLE_MODAL,
@@ -69,6 +103,11 @@ export const modal = (index, name) => {
     };
 };
 
+/**
+ * Function to set errors state. In payload we're passing object with errors and array with errors messages.
+ * @param    {object} err    Object with errors
+ * @param    {object} msg    Array with error messages
+ */
 export const errors = (err, msg) => {
     return {
         type: ACTION_TYPES.SET_ERRORS,
