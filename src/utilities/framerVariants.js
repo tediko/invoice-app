fb22f973506070419f35de7d047a5dcb9a76c5d9
tediko <tediko123@wp.pl>
2021-08-21 13:48:37 +0200

    Change: Move all component elements variants to one object

--- src/utilities/framerVariants.js
+++ src/utilities/framerVariants.js
@@ -1,68 +1,142 @@
 // FormController variants
-export const backdropVariants = {
-    hidden: {
-        opacity: 0,
+export const FormControllerVariants = {
+    backdrop: {
+        hidden: {
+            opacity: 0,
+        },
+        visible: {
+            opacity: 1,
+            transition: { duration: 0.3 },
+        },
+        exit: {
+            opacity: 0,
+            transition: { duration: 0.3 },
+        },
     },
-    visible: {
-        opacity: 1,
-        transition: { duration: 0.3 },
+    form: {
+        hidden: {
+            x: '-100vw',
+        },
+        visible: {
+            x: 0,
+            transition: { type: 'spring', duration: 1, bounce: 0.2 },
+        },
+        exit: {
+            x: '-100vw',
+            transition: { duration: 1 },
+        },
     },
-    exit: {
-        opacity: 0,
-        transition: { duration: 0.3 },
-    },
-};
-
-export const formVariants = {
-    hidden: {
-        x: '-100vw',
-    },
-    visible: {
-        x: 0,
-        transition: { type: 'spring', duration: 1, bounce: 0.2 },
-    },
-    exit: {
-        x: '-100vw',
-        transition: { duration: 1 },
+    reduced: {
+        hidden: {
+            opacity: 0,
+        },
+        visible: {
+            opacity: 1,
+            transition: { duration: 0.5, delay: 0.1 },
+        },
+        exit: {
+            opacity: 0,
+        },
     },
 };
 
 // Modal variants
 export const modalVariants = {
-    hidden: {
-        opacity: 0,
+    modal: {
+        hidden: {
+            opacity: 0,
+        },
+        visible: {
+            opacity: 1,
+            transition: { duration: 0.3 },
+        },
+        exit: {
+            opacity: 0,
+        },
     },
-    visible: {
-        opacity: 1,
-        transition: { duration: 0.3 },
+    container: {
+        hidden: {
+            scale: 0,
+        },
+        visible: {
+            scale: [0, 1.1, 1],
+            transition: { duration: 0.5, delay: 0.1 },
+        },
     },
-    exit: {
-        opacity: 0,
-    },
-};
-
-export const modalContainerVariants = {
-    hidden: {
-        scale: 0,
-    },
-    visible: {
-        scale: [0, 1.1, 1],
-        transition: { duration: 0.5, delay: 0.1 },
+    reduced: {
+        hidden: {
+            opacity: 0,
+        },
+        visible: {
+            opacity: 1,
+            transition: { duration: 0.5, delay: 0.1 },
+        },
+        exit: {
+            opacity: 0,
+        },
     },
 };
 
 // Invoices variants
-export const invoicesHeaderVariants = {
-    hidden: { opacity: 0, x: -50 },
-    visible: {
-        opacity: 1,
-        x: 0,
-        transition: {
-            duration: 1,
+export const invoicesVariants = {
+    header: {
+        hidden: { opacity: 0, x: -50 },
+        visible: {
+            opacity: 1,
+            x: 0,
+            transition: {
+                duration: 1,
+            },
+        },
+        exit: {
+            opacity: 0,
         },
     },
-    exit: {
-        opacity: 0,
+    list: (index) => {
+        return {
+            hidden: {
+                y: 10,
+                opacity: 0,
+            },
+            visible: {
+                y: 0,
+                opacity: 1,
+                transition: {
+                    type: 'spring',
+                    delay: 0.1 * index,
+                },
+            },
+            exit: {
+                y: 10,
+                opacity: 0,
+                transition: {
+                    type: 'spring',
+                    delay: 0.05 * index,
+                    duration: 0.45,
+                },
+            },
+        };
+    },
+    errorMessage: {
+        hidden: { x: -50, opacity: 0 },
+        visible: {
+            x: 0,
+            opacity: 1,
+            transition: { type: 'spring' },
+        },
+        exit: { opacity: 0 },
+    },
+    reduced: {
+        hidden: {
+            opacity: 0,
+        },
+        visible: {
+            opacity: 1,
+            transition: { duration: 0.5, delay: 0.1 },
+        },
+        exit: {
+            opacity: 0,
+        },
     },
 };
 
@@ -92,69 +166,106 @@ export const invoicesListVariants = (index) => {
     };
 };
 
-export const invoicesErrorMessageVariants = {
-    hidden: { x: -50, opacity: 0 },
-    visible: {
-        x: 0,
-        opacity: 1,
-        transition: { type: 'spring' },
-    },
-    exit: { opacity: 0 },
-};
-
 // InvoiceView variants
-export const invoiceViewLinkVariants = {
-    hidden: { x: -20, opacity: 0 },
-    visible: { x: 0, opacity: 1, transition: { type: 'spring', duration: 1 } },
-    exit: {
-        x: -20,
-        opacity: 0,
+export const invoiceViewVariants = {
+    link: {
+        hidden: { x: -20, opacity: 0 },
+        visible: {
+            x: 0,
+            opacity: 1,
+            transition: { type: 'spring', duration: 1 },
+        },
+        exit: {
+            x: -20,
+            opacity: 0,
+        },
     },
-};
-
-export const invoiceViewControllerVariants = {
-    hidden: { y: -20, opacity: 0 },
-    visible: { y: 0, opacity: 1, transition: { type: 'spring', duration: 1 } },
-    exit: { opacity: 0, duration: 1 },
+    controller: {
+        hidden: { y: -20, opacity: 0 },
+        visible: {
+            y: 0,
+            opacity: 1,
+            transition: { type: 'spring', duration: 1 },
+        },
+        exit: { opacity: 0, duration: 1 },
+    },
+    reduced: {
+        hidden: {
+            opacity: 0,
+        },
+        visible: {
+            opacity: 1,
+            transition: { duration: 0.5, delay: 0.1 },
+        },
+        exit: {
+            opacity: 0,
+        },
+    },
 };
 
 // InvoiceInfo variants
 export const invoiceInfoVariants = {
-    hidden: { scale: 0.9, opacity: 0 },
-    visible: {
-        scale: 1,
-        opacity: 1,
-        transition: { type: 'spring', duration: 1 },
+    info: {
+        hidden: { scale: 0.9, opacity: 0 },
+        visible: {
+            scale: 1,
+            opacity: 1,
+            transition: { type: 'spring', duration: 1 },
+        },
+        exit: { opacity: 0 },
     },
-    exit: { opacity: 0 },
+    reduced: {
+        hidden: {
+            opacity: 0,
+        },
+        visible: {
+            opacity: 1,
+            transition: { duration: 0.5, delay: 0.1 },
+        },
+        exit: {
+            opacity: 0,
+        },
+    },
 };
 
 // RouteError
 export const routeErrorVariants = {
-    hidden: { opacity: 0 },
-    visible: { opacity: 1, transition: { duration: 1 } },
-    exit: { opacity: 0 },
-};
-
-export const routeErrorIllustrationVariants = {
-    hidden: { scale: 0.9 },
-    visible: { scale: 1, transition: { duration: 1 } },
-    exit: { scale: 0.8 },
-};
-
-export const routeErrorTitleVariants = {
-    hidden: { x: -100, opacity: 0 },
-    visible: {
-        x: 0,
-        opacity: 1,
-        transition: {
-            type: 'spring',
-            bounce: 0.8,
-            delay: 0.3,
-            duration: 1,
+    routeError: {
+        hidden: { opacity: 0 },
+        visible: { opacity: 1, transition: { duration: 1 } },
+        exit: { opacity: 0 },
+    },
+    illustration: {
+        hidden: { scale: 0.9 },
+        visible: { scale: 1, transition: { duration: 1 } },
+        exit: { scale: 0.8 },
+    },
+    title: {
+        hidden: { x: -100, opacity: 0 },
+        visible: {
+            x: 0,
+            opacity: 1,
+            transition: {
+                type: 'spring',
+                bounce: 0.8,
+                delay: 0.3,
+                duration: 1,
+            },
+        },
+        exit: { scale: 0.8 },
+    },
+    reduced: {
+        hidden: {
+            opacity: 0,
+        },
+        visible: {
+            opacity: 1,
+            transition: { duration: 0.5, delay: 0.1 },
+        },
+        exit: {
+            opacity: 0,
         },
     },
-    exit: { scale: 0.8 },
 };
 
 // Motion reduced variants
