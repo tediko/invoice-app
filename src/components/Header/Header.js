163224122a702b40a915af2854fa471862f1d5f5
tediko <tediko123@wp.pl>
2021-08-23 17:18:54 +0200

    Fix: Bug where Invoices doesn't render when user click fast two times on Logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import Icon from '../shared/Icon/Icon';
 import { useTheme } from 'styled-components';
 import { StyledHeader, Logo, ThemeToggle, Profile } from './HeaderStyles';
@@ -6,10 +7,23 @@ import { useGlobalContext } from '../App/context';
 const Header = () => {
     const { colors } = useTheme();
     const { theme, toggleTheme, discard } = useGlobalContext();
+    const isClickable = useRef(true);
+
+    // Function to prevent too fast clicking on Logo since it was causing a bug
+    // with Framer Motion where two fast clicks cause that component Invoices doesn't render.
+    const handleClick = (event) => {
+        if (!isClickable.current) {
+            event.preventDefault();
+        } else {
+            isClickable.current = false;
+            setTimeout(() => (isClickable.current = true), 1000);
+            discard();
+        }
+    };
 
     return (
         <StyledHeader>
-            <Logo aria-label="Home Page" to="/" onClick={discard} />
+            <Logo aria-label="Home Page" to="/" onClick={handleClick} />
             <ThemeToggle aria-label="Theme toggle" onClick={toggleTheme}>
                 <Icon
                     name={theme === 'light' ? 'moon' : 'sun'}
